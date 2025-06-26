67e1142222c444ae34eafb7faa2c2eb530468c85
BosEriko <bos@eriko.ph>
2025-06-26 15:25:06 +0800

    Feature: Remove NEXT_PUBLIC_SITE_NAME

--- app/_utilities/env.js
+++ app/_utilities/env.js
@@ -1,7 +1,6 @@
 // Note: Declared ENV variables must start with "NEXT_PUBLIC_" to work in the browser
 // More Information: https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#bundling-environment-variables-for-the-browser
 const env = {
-  siteName: process.env.NEXT_PUBLIC_SITE_NAME,
   websocket: process.env.NEXT_PUBLIC_WEBSOCKET,
 }
 
