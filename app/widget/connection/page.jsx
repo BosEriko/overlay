94c1d91383bcb7521742c2357a6529e1af88e7fc
BosEriko <bos@eriko.ph>
2025-06-27 20:45:00 +0800

    Feature: Hide Connection Component when connected

--- app/widget/connection/page.jsx
+++ app/widget/connection/page.jsx
@@ -4,11 +4,13 @@ import { useWebSocket } from '../../_hooks/useWebsocket';
 const WebSocketStatus = () => {
   const { isConnected } = useWebSocket();
 
+  if (isConnected) return null;
+
   return (
     <div className="h-[1080px] w-[1920px]">
-        <div className={`m-5 px-3 py-2 rounded-xl inline-block shadow-lg text-white font-semibold ${isConnected ? 'bg-green-600' : 'bg-red-600'}`}>
-        {isConnected ? '✅ Connected' : '❌ Disconnected'}
-        </div>
+      <div className="m-5 px-3 py-2 rounded-xl inline-block shadow-lg text-white font-semibold bg-red-600">
+        ❌ Disconnected
+      </div>
     </div>
   );
 };
