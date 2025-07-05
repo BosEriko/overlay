a9d68f1afcb77a8ea48d45237c42370a44a8e5c4
BosEriko <bos@eriko.ph>
2025-07-06 03:02:07 +0800

    Feature: Create ID for Shoutout

--- app/widget/feed/page.jsx
+++ app/widget/feed/page.jsx
@@ -26,7 +26,7 @@ function playSound() {
   });
 }
 
-function ToastContent({ children, className = '' }) {
+function ToastContent({ children, className = '', type }) {
   const [exiting, setExiting] = useState(false);
 
   useEffect(() => {
@@ -34,10 +34,14 @@ function ToastContent({ children, className = '' }) {
     return () => clearTimeout(timeout);
   }, []);
 
+  const toastContentClassMap = {
+    event: 'max-w-sm border-[5px] py-2 px-3',
+    chat: 'max-w-sm border-[5px] py-2 px-3',
+    shoutout: 'max-w-md border-[0px]',
+  };
+
   return (
-    <div
-      className={`${exiting ? 'animate-slide-out' : 'animate-slide-in'} ${pixelify.className} ${className} max-w-sm w-full shadow-xl py-2 px-3 pointer-events-auto border-[5px] text-xl rounded-2xl mb-[2px]`}
-    >
+    <div className={`${exiting ? 'animate-slide-out' : 'animate-slide-in'} ${pixelify.className} ${className} ${toastContentClassMap[type]} w-full shadow-xl pointer-events-auto text-xl rounded-2xl mb-[2px] overflow-hidden`}>
       {children}
     </div>
   );
@@ -57,7 +61,7 @@ const pastelThemes = [
   { gradient: 'from-fuchsia-200 to-sky-200', textColor: 'text-fuchsia-900', iconColor: 'text-fuchsia-800' },
 ];
 
-function showToast({ type, message, username }) {
+function showToast({ type, message, username, user }) {
   const position = 'bottom-left';
   const duration = 5000;
 
@@ -67,31 +71,51 @@ function showToast({ type, message, username }) {
   }
 
   const isEvent = type === 'event';
-  const icon = isEvent ? faStar : faComment;
+  const isShoutout = type === 'shoutout';
 
-  const theme = isEvent
-    ? pastelThemes[Math.floor(Math.random() * pastelThemes.length)]
-    : { gradient: '', textColor: 'text-yellow-800', iconColor: 'text-yellow-700' };
+  const icon = isEvent ? faStar : faComment;
+  const theme = isEvent ? pastelThemes[Math.floor(Math.random() * pastelThemes.length)] : { gradient: '', textColor: 'text-yellow-800', iconColor: 'text-yellow-700' };
+  const toastClassMap = {
+    event: `bg-gradient-to-br ${theme.gradient} ${theme.textColor} border-white`,
+    chat: 'border-yellow-500 bg-yellow-300 text-yellow-800',
+    shoutout: 'border-yellow-300 bg-gradient-to-br from-yellow-300 to-yellow-500 text-yellow-800',
+  };
 
   const id = toast.custom(
     () => (
-      <ToastContent
-        className={
-          isEvent
-            ? `bg-gradient-to-br ${theme.gradient} ${theme.textColor} border-white`
-            : 'border-yellow-500 bg-yellow-300 text-yellow-800'
-        }
-      >
-        <div className="flex items-start gap-3 overflow-hidden">
-          <FontAwesomeIcon
-            icon={icon}
-            className={`text-2xl shrink-0 mt-1 ${theme.iconColor}`}
-          />
+      <ToastContent className={toastClassMap[type]} type={type}>
+        { !isShoutout && <div className="flex items-start gap-3">
+          <FontAwesomeIcon icon={icon} className={`text-2xl shrink-0 mt-1 ${theme.iconColor}`} />
           <div>
             {type === 'chat' && <p className="font-bold text-yellow-700">{username}</p>}
             <p>{message}</p>
           </div>
-        </div>
+        </div> }
+        { isShoutout && <div className="flex flex-col gap-3">
+          <div className="flex items-center justify-between pt-3 px-3">
+            <div className="w-10 h-10 rounded-full bg-white flex items-center justify-center"><span className="text-yellow-800">BE</span></div>
+            <div className="border border-white rounded-[100px] text-white py-1 px-2 text-sm capitalize" style={{ backgroundColor: 'rgba(255,255,255,0.25)' }}>{user?.broadcaster_type}</div>
+          </div>
+          <div className="uppercase text-white font-bold text-2xl drop-shadow px-3">Bos Eriko University</div>
+          <div className="flex gap-3 items-center px-3 mb-2">
+            <div><img src={user?.profile_image_url} className="h-30 w-30 rounded-lg border-[5px] border-yellow-500"/></div>
+            <div className="flex flex-col">
+              <div className="font-bold text-2xl text-white drop-shadow">{user?.display_name}</div>
+              <div className="flex gap-1 text-white drop-shadow">
+                <div>ID:</div>
+                <div>BE-{user?.id}</div>
+              </div>
+              <div className="flex gap-1 text-white drop-shadow">
+                <div>Enrolled:</div>
+                <div>{user?.created_at}</div>
+              </div>
+            </div>
+          </div>
+          <div style={{ backgroundColor: "rgba(0,0,0,0.25)" }} className="w-full py-2 px-5 text-sm flex justify-between items-center text-white">
+            <div>twitch.tv/{user?.display_name}</div>
+            <div>BE-{user?.id}</div>
+          </div>
+        </div> }
       </ToastContent>
     ),
     { position, duration }
@@ -110,6 +134,7 @@ export default function FeedWidget() {
         type: wsData.feed_type,
         message: wsData.message,
         username: wsData.username,
+        user: wsData.user || {}
       });
     }
   }, [wsData]);
